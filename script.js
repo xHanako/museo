30214f7b4ef1b35541e2f026c25dda34da4253f4
xHanako <88059871+xHanako@users.noreply.github.com>
2025-04-04 17:47:43 +0000

    Added languaje support.

--- script.js
+++ script.js
@@ -1,18 +1,38 @@
 function verificarCodigo(event) {
-    event.preventDefault(); // Evita que el formulario se envíe automáticamente
+    event.preventDefault();
 
     const codigoIngresado = document.getElementById("password").value;
-    const codigoCorrecto = "2506"; // Código correcto
+    const codigoCorrecto = "2506";
     const errorMessage = document.querySelector(".error-message");
+    const form = document.querySelector(".my-form");
 
     if (codigoIngresado === codigoCorrecto) {
-        window.location.href = "experiencia.html"; // Redirige si el código es correcto
+        sessionStorage.setItem("accesoPermitido", "true");
+        window.open("experiencia.html", "_blank");
+        form.reset();
     } else {
-        errorMessage.style.display = "block"; // Muestra mensaje de error
+        errorMessage.style.display = "block";
+    }
+}
+
+if (window.location.pathname.includes("experiencia.html") || window.location.pathname.includes("experience.html")) {
+    if (sessionStorage.getItem("accesoPermitido") !== "true") {
+        window.location.href = "index.html";
+    } else {
+        sessionStorage.removeItem("accesoPermitido");
     }
 }
 
 function soloNumeros(event) {
     let key = event.key;
-    return /^[0-9]$/.test(key); // Solo permite números del 0 al 9
+    return /^[0-9]$/.test(key);
 }
+
+document.getElementById('start-experience').addEventListener('click', function() {
+    var language = document.getElementById('language').value;
+    if (language === 'es') {
+        window.location.href = 'experiencia.html';
+    } else if (language === 'en') {
+        window.location.href = 'experience.html';
+    }
+});
