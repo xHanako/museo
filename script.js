f7bf1e111b74136647bc157ef6480db6c9c06cf7
xHanako <88059871+xHanako@users.noreply.github.com>
2025-12-06 10:05:15 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function verificarCodigo(event) {
     event.preventDefault();
 
     const codigoIngresado = document.getElementById("password").value;
-    const codigoCorrecto = "7741";
+    const codigoCorrecto = "3483";
     const errorMessage = document.querySelector(".error-message");
     const form = document.querySelector(".my-form");
     const language = document.getElementById('language').value;
